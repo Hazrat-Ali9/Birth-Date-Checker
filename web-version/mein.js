7a6357e308f9bfa2b06742ce613a639be8386b98
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-06-13 10:19:36 +0600

    mein js  🚃

--- web-version/mein.js
+++ web-version/mein.js
@@ -1,4 +1,4 @@
-
+// mein js
 function getDayOfWeek(year, month, day) {
     const date = new Date(year, month - 1, day);
     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
